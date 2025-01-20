18412cade5aeb92581b175719acd7e7ffc67355a
BugHunter-X <askzenai@gmail.com>
2025-01-20 11:04:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* This route is the source of the error */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {return <div>Home</div>}
+function About() {return <div>About</div>}
+function NotFound() { return <div>404 Not Found</div>}
